0178f9e6a307d4f73b2fc73c3dd759f3adb99723
yosri abdedayem <abdedayemyosri@gmail.com>
2020-05-09 03:15:57 +0200

    add unit validation

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -8,65 +8,120 @@ use Illuminate\Support\Facades\Session;
 
 class UnitController extends Controller
 {
-    public function index(){
-        $units=Unit::paginate((env('PAGINATION_COUNT')));
+    public function index()
+    {
+        $units = Unit::paginate((env('PAGINATION_COUNT')));
 
         return view('admin.units.units')->with(
-            ['units'=>$units]
+            ['units' => $units]
         );
     }
 
-    public function store(Request $request){
-         dd($request);
+    private function unitNameExists($unitName)
+    {
+        $unit = Unit::where(
+            'unit_name', '=', $unitName
+        )->first();
+        if (!is_null($unit)) {
+            Session::flash('message', 'Unit Name(' . $unitName . ') already exists');
+            return false;
+
+
+        }
+        return true;
+    }
+
+    private function unitCodeExists($unitCode)
+    {
+        $unit = Unit::where(
+            'unit_code', '=', $unitCode
+        )->first();
+        if (!is_null($unit)) {
+            Session::flash('message', 'Unit Name(' . $unitCode . ') already exists');
+            return false;
+        }
+        return true;
+    }
+
+
+    public function store(Request $request)
+    {
         //TODO check if unit exist
         $request->validate([
-            'unit_name'=>'required',
-            'unit_code'=>'required'
+            'unit_name' => 'required',
+            'unit_code' => 'required'
         ]);
-        $unit=new Unit();
-        $unit->unit_name=$request->input('unit_name');
-        $unit->unit_code=$request->input('unit_code');
-        $unit->save();
-        Session::flash('message','Unit '.$unit->unit_name .' has been added');
-        return redirect()->back();
-    }
+        $unitName = $request->input('unit_name');
+        $unitCode = $request->input('unit_code');
 
-    public function update(Request $request){
-    $request->validate([
-        'unit_code'=>'required',
-        'unit_id'=>'required',
-        'unit_name'=>'required'
-    ]);
-    $unitID=intval($request->input('unit_id'));
-
-    $unit=Unit::find($unitID);
-
-    $unit->unit_name=$request->input('unit_name');
-    $unit->unit_code=$request->input('unit_code');
-    $unit->save();
-    Session::flush('message','Unit '.$unit->unit_name .' has been updated');
-    return redirect()->back();
-
-    }
-    public function search(Request $request){
-
-    }
-
-    public function delete(Request $request){
-
-
-        if(is_null($request->input('unit_id'))||empty($request->input('unit_id'))){
-            Session::flush('message','Unit ID is required');
+        if (! $this->unitNameExists($unitName)){
+            return redirect()->back();
+        }
+        if (!$this->unitCodeExists($unitCode)){
             return redirect()->back();
         }
 
 
 
-        Unit::destroy($request->input('unit_id'));
-        Session::flash('message','Unit  has been removed');
+
+        $unit = new Unit();
+        $unit->unit_name = $request->input('unit_name');
+        $unit->unit_code = $request->input('unit_code');
+        $unit->save();
+        Session::flash('message', 'Unit ' . $unit->unit_name . ' has been added');
         return redirect()->back();
     }
 
+    public function update(Request $request)
+    {
+        $request->validate([
+            'unit_code' => 'required',
+            'unit_id' => 'required',
+            'unit_name' => 'required'
+        ]);
+
+        $unitName = $request->input('unit_name');
+        $unitCode = $request->input('unit_code');
+
+        if (! $this->unitNameExists($unitName)){
+            return redirect()->back();
+        }
+        if (!$this->unitCodeExists($unitCode)){
+            return redirect()->back();
+        }
+
+
+        $unitID = intval($request->input('unit_id'));
+
+        $unit = Unit::find($unitID);
+
+        $unit->unit_name = $request->input('unit_name');
+        $unit->unit_code = $request->input('unit_code');
+        $unit->save();
+        Session::flash('message', 'Unit ' . $unit->unit_name . ' has been updated');
+        return redirect()->back();
+
+    }
+
+    public function search(Request $request)
+    {
+
+    }
+
+    public function delete(Request $request)
+    {
+
+
+        if (is_null($request->input('unit_id')) || empty($request->input('unit_id'))) {
+            Session::flush('message', 'Unit ID is required');
+            return redirect()->back();
+        }
+
+
+        Unit::destroy($request->input('unit_id'));
+        Session::flash('message', 'Unit  has been removed');
+        return redirect()->back();
+    }
 
 
 }
