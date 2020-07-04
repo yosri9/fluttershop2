0376cfeda32c6781ae13c9c526a707c7339f42af
abdedayem yosri <60704101+yosri9@users.noreply.github.com>
2020-07-05 00:33:00 +0200

    maximize the number of row

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Address::class,100)->create();
-        factory(\App\User::class,50)->create();
-        factory(\App\Product::class,150)->create();
-        factory(\App\Image::class,350)->create();
-        factory(\App\Review::class,350)->create();
+        factory(\App\Address::class,1000)->create();
+        factory(\App\User::class,500)->create();
+        factory(\App\Product::class,1500)->create();
+        factory(\App\Image::class,3500)->create();
+        factory(\App\Review::class,3500)->create();
         factory(\App\Category::class,50)->create();
-        factory(\App\Tag::class,15)->create();
-         factory(\App\Ticket::class ,15)->create();
+        factory(\App\Tag::class,150)->create();
+         factory(\App\Ticket::class ,150)->create();
 
     }
 }
