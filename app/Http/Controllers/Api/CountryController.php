08afc2a8225d9a91600fed8ef4c6e9409d2678db
abdedayem yosri <60704101+yosri9@users.noreply.github.com>
2020-07-13 18:29:21 +0200

    add paginate to cities and states

--- app/Http/Controllers/Api/CountryController.php
+++ app/Http/Controllers/Api/CountryController.php
@@ -16,12 +16,12 @@ class CountryController extends Controller
     }
     public function showStates($id){
         $country=Country::find($id);
-        return StateResource::collection($country->states);
+        return StateResource::collection($country->states()->paginate());
     }
 
     public function showCities($id){
         $country=Country::find($id);
-        return CityResource::collection($country->cities);
+        return CityResource::collection($country->cities()->paginate());
 
     }
 }
